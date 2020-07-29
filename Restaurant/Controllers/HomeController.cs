9a524d09ae16717cf5115bbb44d936dd673534e2
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-07-29 11:49:47 -0700

    Remove s from end of Controller in HomeController.
    Co-authored-by: Taylor Somers <tsomers@yandex.com>
    Co-authored-by: Erich Richter <erich.richter@gmail.com>

--- Restaurant/Controllers/HomeController.cs
+++ Restaurant/Controllers/HomeController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Restaurant.Controllers
 {
-  public class HomeController : Controllers
+  public class HomeController : Controller
   {
       [HttpGet("/")]
       public ActionResult Index()
